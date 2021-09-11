53bdd706eebe18a52fe13d61d43cad782d49a41f
Novakq <59772186+Novakq@users.noreply.github.com>
2021-09-11 19:47:35 +0200

    Update guestbook.js

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://ef5f4d14.us-south.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
